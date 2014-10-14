6d0db7b3c19178193647d1439f79f68170783a2f
AlexeyZhuravlev <aa_Zhur@mail.ru>
2014-10-14 23:39:26 +0400

    Synonyms and antonyms resources  + some lang_utils and fileresource fixes

---
+++ resources/antonyms/raw_data/extract_antonyms_raw.py
@@ -0,0 +1,8 @@
+﻿__author__ = 'Алексей'
+
+import codecs
+from resources.synonyms import import_from_site
+
+f = codecs.open('antonyms_raw.txt', mode='w', encoding='utf-8')
+site = 'http://antonymonline.ru/'
+import_from_site(site, f)
