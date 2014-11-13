c04855070370b06e6c14e01f9f824bbf29fa476a
AlexeyZhuravlev <aa_Zhur@mail.ru>
2014-11-13 22:47:12 +0300

    Added collocation inflection. Rebuilt assets

--- preparation/resources/collocations/parse_collocations.py
+++ preparation/resources/collocations/parse_collocations.py
@@ -5,12 +5,36 @@ from preparation.resources.collocations import _raw_data
 from preparation.resources.Resource import gen_resource
 from hb_res.explanations import Explanation
 from preparation import modifiers
+from preparation.lang_utils.morphology import morph
+import copy
+
+@modifiers.modifier_factory
+def inflect_first_word():
+    def apply(e: Explanation):
+        ret = copy.copy(e)
+        words = e.text.split(' ')
+        if words[1] == '*пропуск*':
+            second = morph.parse(e.title)[0]
+            first = morph.parse(words[0])[0]
+            if second.tag.case is None or second.tag.gender is None or second.tag.number is None:
+                return e
+            first = first.inflect({second.tag.case,
+                                   second.tag.gender,
+                                   second.tag.number})
+            if first is None:
+                return e
+            ret.text = first.word + ' *пропуск*'
+            return ret
+        else:
+            return e
+    return apply
 
 collocations_mods = [
     modifiers.gapanize_title(),
     modifiers.normalize_title(),
     modifiers.shadow_cognates(5, '\W+'),
     modifiers.check_contains_valid_parts(1, 0.1, '\W+'),
+    inflect_first_word(),
     modifiers.calculate_key()
 ]
 
