4ec7b0d4e1739a61fdbbe4fa2e50905ba13667b6
mike <surinmike@gmail.com>
2014-11-23 16:22:18 +0300

    improved infrastructure

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages, findall  # Always prefer setuptools over distutils
+from preparation.tools.build_assets import main
+
+# build all assets
+main(['all'])
 
 setup(
     name='hatbot_resources',
