9fde4f6190154640813df1ab7ad3385a4df1f163
mike <surinmike@gmail.com>
2014-11-23 18:25:20 +0300

    resource dependencies

--- preparation/resources/Resource.py
+++ preparation/resources/Resource.py
@@ -2,7 +2,47 @@ __author__ = "mike"
 
 _resource_blacklist = {'Resource'}
 _registered_resources = dict()
+_built_resources = set()
 
+from hb_res.storage import get_storage
+
+def build_deps(res_obj):
+    assert hasattr(res_obj, 'dependencies')
+    for dep in res_obj.dependencies:
+        assert dep + 'Resource' in _registered_resources
+        _registered_resources[dep + 'Resource']().build()
+
+def applied_modifiers(res_obj):
+    for explanation in res_obj:
+        r = copy(explanation)
+        for functor in resource.modifiers:
+            if r is None:
+                break
+            r = functor(r)
+        if r is not None:
+            yield r
+
+def generate_asset(res_obj, out_storage):
+    out_storage.clear()
+    for explanation in applied_modifiers(res_obj):
+        out_storage.add_entry(explanation)
+
+
+def resource_build(res_obj):
+    res_name = res_obj.__class__.__name__
+    assert res_name.endswith('Resource')
+    res_name = res_name[:-len('Resource')]
+
+    if res_name in _built_resources: 
+        return
+    
+    build_deps(res_obj)
+    _built_resources.add(res_name)
+    
+    with get_storage(res_name) as out_storage:
+        print("Starting {} generation".format(res_name))
+        generate_asset(res_obj, out_storage)
+        print("Finished {} generation".format(res_name))
 
 class ResourceMeta(type):
     """
@@ -15,21 +55,22 @@ class ResourceMeta(type):
         global _registered_resources
         if name in _registered_resources.keys():
             raise KeyError('Resource with name {} is already registered'.format(name))
+        dct['build'] = resource_rebuild
         res = super(ResourceMeta, mcs).__new__(mcs, name, bases, dct)
         if name not in _resource_blacklist:
             _registered_resources[name] = res
         return res
-
 
 class Resource(metaclass=ResourceMeta):
     def __iter__(self):
         raise NotImplementedError
 
 
-def gen_resource(res_name, modifiers):
+def gen_resource(res_name, modifiers, dependencies=[]):
     def decorator(func):
         def __init__(self):
             self.modifiers = modifiers
+            self.dependencies = dependencies
 
         def __iter__(self):
             return iter(func())
