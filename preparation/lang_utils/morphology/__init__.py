63d2fd998d57cd1eebca48858f115c54269aac33
AlexeyZhuravlev <aa_Zhur@mail.ru>
2014-11-23 16:46:46 +0300

    Added functionality

--- preparation/lang_utils/morphology/__init__.py
+++ preparation/lang_utils/morphology/__init__.py
@@ -8,3 +8,4 @@ morph = pymorphy2.MorphAnalyzer()
 from .word_forms import get_valid_noun_initial_form, get_initial_forms
 from .parts_of_speech import get_parts_of_speech
 from .parts_of_speech import is_remarkable
+from .replace_with_gap import replace_noun_with_pronoun, replace_noun_with_question
--- preparation/lang_utils/morphology/replace_with_gap.py
+++ preparation/lang_utils/morphology/replace_with_gap.py
@@ -3,3 +3,55 @@ __author__ = 'shkiper'
 from . import morph
 
 
+def get_best_parse(word: str, part: str):
+    possibilities = [x for x in morph.parse(word) if x.tag.POS == part]
+    if len(possibilities) == 0:
+        return None
+    else:
+        return max(possibilities, key=lambda x: x.score)
+
+WHO_PARSE = get_best_parse('кто', "NPRO")
+WHAT_PARSE = get_best_parse('что', "NPRO")
+HE_PARSE = get_best_parse('он', "NPRO")
+SHE_PARSE = get_best_parse('она', "NPRO")
+IT_PARSE = get_best_parse('оно', "NPRO")
+THEY_PARSE = get_best_parse('они', "NPRO")
+
+
+def add_question_symbol(func):
+    def addition(word, default=None):
+        return func(word, default) + "?"
+    return addition
+
+
+@add_question_symbol
+def replace_noun_with_question(word: str, default: str=None):
+    noun_parse = get_best_parse(word, "NOUN")
+    if noun_parse is None:
+        return default
+    if noun_parse.tag.case is None:
+        return default
+    if noun_parse.tag.animacy == "anim":
+        return WHO_PARSE.inflect({noun_parse.tag.case}).word
+    else:
+        return WHAT_PARSE.inflect({noun_parse.tag.case}).word
+
+
+def replace_noun_with_pronoun(word: str, default: str=None):
+    noun_parse = get_best_parse(word, "NOUN")
+    if noun_parse is None:
+        return default
+    if noun_parse.tag.case is None:
+        return default
+    case = noun_parse.tag.case
+    gender = noun_parse.tag.gender
+    number = noun_parse.tag.number
+    if number == 'plur':
+        return THEY_PARSE.inflect({case}).word
+    if gender == 'neut':
+        return IT_PARSE.inflect({case}).word
+    if gender == 'femn':
+        return SHE_PARSE.inflect({case}).word
+    if gender == 'macs':
+        return HE_PARSE.inflect({case}).word
+    return default
