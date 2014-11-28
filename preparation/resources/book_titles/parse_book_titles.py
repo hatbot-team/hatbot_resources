698313dac943edf19ddcacc272f9fb137b8801ef
Maxim Rusak <mak-rusak@yandex.ru>
2014-11-28 05:40:50 +0400

    add prior rating to books and films, authors to books

--- preparation/resources/book_titles/parse_book_titles.py
+++ preparation/resources/book_titles/parse_book_titles.py
@@ -16,23 +16,37 @@ def add_common_prefix():
         return ret
     return apply
 
+@modifiers.modifier_factory
+def add_author():
+    def apply(e: Explanation):
+        ret = copy.copy(e)
+        ret.text = ret.text + " Автор: " + e.author
+        return ret
+    return apply
+
+
 book_titles_mods = [
     modifiers.check_contains_valid_parts(2, 0.1, '\W+'),
     modifiers.shadow_title_with_question(),
     modifiers.normalize_title(),
+    add_common_prefix(),
+    add_author(),
     modifiers.shadow_cognates(5, '\W+'),
     modifiers.delete_multiple_gaps(0),
-    add_common_prefix(),
     modifiers.calculate_key()
 ]
 
 @gen_resource('BookTitlesResource', book_titles_mods)
 def read_data():
     with open(_raw_data, 'r', encoding='utf-8') as source:
+        count = 0
         while True:
-            title = source.readline()
-            author = source.readline()
+            count += 1
+            title = source.readline().strip('\n')
+            author = source.readline().strip('\n')
             if not title: break
             for word in re.split('\W+', title):
                 if len(word) > 0:
-                    yield Explanation(word, title.strip('\n'))
+                    explanation = Explanation(title = word, text = title, prior_rating = 1.0/count)
+                    explanation.author = author
+                    yield explanation
