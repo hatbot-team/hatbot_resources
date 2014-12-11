963275a084ef61b226364dacaabd5ac48703998a
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-12-11 07:58:29 +0300

    Fix repetitions in crosswords

--- preparation/resources/crosswords/parse_crosswords.py
+++ preparation/resources/crosswords/parse_crosswords.py
@@ -7,15 +7,20 @@ from hb_res.explanations import Explanation
 from preparation import modifiers
 
 crosswords_mods = [
-    modifiers.strip('.', target_field='title'),
+    modifiers.strip(target_field='title'),
+    modifiers.strip(),
     modifiers.normalize_title(),
     modifiers.calculate_key()
 ]
 
+
 @gen_resource('CrosswordsResource', crosswords_mods)
 def read_data():
+    met = set()
     with open(_raw_data, 'r', encoding='utf-8') as source:
         for line in source:
             tokens = line.split('$')
-            word, text = tokens[1], tokens[2]
-            yield Explanation(word.strip().lower(), text.strip())
+            word_and_text = tokens[1], tokens[2]
+            if word_and_text not in met:
+                met.add(word_and_text)
+                yield Explanation(*word_and_text)
