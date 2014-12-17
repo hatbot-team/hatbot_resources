05ef5d0a401c2ad28317c20fd3bed13724308ff2
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-12-17 12:20:32 +0300

    minor

--- tests/test_text.py
+++ tests/test_text.py
@@ -12,14 +12,14 @@ __author__ = 'moskupols'
 def test_text_starts_with_non_ws(trunk):
     case = unittest.TestCase()
     for e in asset_cache(trunk):
-        case.assertFalse(e.text.startswith(' '), e)
+        case.assertFalse(e.text.startswith(' '), repr(e))
 
 
 @trunk_parametrized()
 def test_text_ends_with_non_ws(trunk):
     case = unittest.TestCase()
     for e in asset_cache(trunk):
-        case.assertFalse(e.text.endswith(' '), e)
+        case.assertFalse(e.text.endswith(' '), repr(e))
 
 
 @trunk_parametrized()
@@ -27,30 +27,30 @@ def test_spaces_near_punct(trunk):
     case = unittest.TestCase()
     for e in asset_cache(trunk):
         # there should be no space before punctuation
-        case.assertNotRegex(e.text, r' [.!?,:;)]', e)
+        case.assertNotRegex(e.text, r' [.!?,:;)]', repr(e))
 
         # should not be after some
-        case.assertFalse('( ' in e.text, e)
+        case.assertFalse('( ' in e.text, repr(e))
 
         # and should after some
-        case.assertNotRegex(e.text, r'[!?]\w', e)
-        case.assertNotRegex(e.text, r'[,:;]([^ ]|$)', e)
+        case.assertNotRegex(e.text, r'[!?]\w', repr(e))
+        case.assertNotRegex(e.text, r'[,:;]([^ ]|$)', repr(e))
         # had to circumvent 'Лента.ру' exception
-        case.assertNotRegex(e.text, r'(?<![Лл]ен)[ёа-я]{2}[.][ЁА-Яёа-я]', e)
+        case.assertNotRegex(e.text, r'(?<![Лл]ен)[ёа-я]{2}[.][ЁА-Яёа-я]', repr(e))
 
 
 @trunk_parametrized()
 def test_single_spacing(trunk):
     case = unittest.TestCase()
     for e in asset_cache(trunk):
-        case.assertFalse('  ' in e.text, e)
+        case.assertFalse('  ' in e.text, repr(e))
 
 
 @trunk_parametrized()
 def test_no_ws_but_space(trunk):
     case = unittest.TestCase()
     for e in asset_cache(trunk):
-        case.assertNotRegex(e.text, r'(?! )\s', e)
+        case.assertNotRegex(e.text, r'(?! )\s', repr(e))
 
 
 if __name__ == '__main__':
